e97b537078dfb59016f577fe5c33f1d6d75518f7
Lianna <liannanovitz@gmail.com>
2024-03-26 10:11:16 -0700

    add passing tests

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -8,15 +8,20 @@ export function getFib(n: Number, arr: Array<Number>) {
   return (Number(arr[arr.length - 2]) + Number(arr[arr.length - 1]) );
 }
 
+export function callFib(n: Number) {
+  let sequence = new Array;
+  for (let i = 0; i < Number(n) + 1; i++) {
+    sequence.push(getFib(i, sequence));
+  }
+  return sequence
+}
+
 export async function POST(request: Request) {
   const formData = await request.formData()
   const value = parseInt(formData.get('fibN') as string)
 
   if (!value) return Response.json({"response":"404"})
   
-  let sequence = new Array;
-  for (let i = 0; i < value + 1; i++) {
-    sequence.push(getFib(i, sequence));
-  }
-return Response.json({ sequence })
+  let sequence = callFib(value);
+  return Response.json({ sequence })
 }
