3a54ca3a97009be84a01d4c1acbb86645aca0ef2
Lianna <liannanovitz@gmail.com>
2024-04-02 14:09:43 -0700

    actually wrap in suspense

--- src/app/display/page.tsx
+++ src/app/display/page.tsx
@@ -4,10 +4,16 @@ import { useSearchParams } from 'next/navigation'
 import { callFib } from '../utils/fib_utils';
 import { Suspense } from 'react'
 
-export default function Page() {
-    const searchParams = useSearchParams();
+// https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+function Search() {
+    // here you look up cache and if not there, callFib
+    const searchParams = useSearchParams()
     const input = parseInt(searchParams.get('name') as string);
     const sequence = callFib(input).toString()
-    // here you look up cache and if not there, callFib
-    return (<Suspense><div className="flex min-h-screen flex-col items-center justify-between p-24">{sequence}</div></Suspense>)
+   
+    return <div className="flex min-h-screen flex-col items-center justify-between p-24">{sequence}</div>
+  }
+
+export default function Page() {
+    return (<Suspense><Search/></Suspense>)
 }
