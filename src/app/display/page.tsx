996070c3f851d01e48d2a7e6d060696197a93a59
Lianna <liannanovitz@gmail.com>
2024-04-02 14:04:51 -0700

    allow rendering pre fetch of search params

--- src/app/display/page.tsx
+++ src/app/display/page.tsx
@@ -2,10 +2,12 @@
 
 import { useSearchParams } from 'next/navigation'
 import { callFib } from '../utils/fib_utils';
+import { Suspense } from 'react'
 
 export default function Page() {
     const searchParams = useSearchParams();
     const input = parseInt(searchParams.get('name') as string);
     const sequence = callFib(input).toString()
-    return (<div className="flex min-h-screen flex-col items-center justify-between p-24">{sequence}</div>)
+    // here you look up cache and if not there, callFib
+    return (<Suspense><div className="flex min-h-screen flex-col items-center justify-between p-24">{sequence}</div></Suspense>)
 }
